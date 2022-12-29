fdfe64b9cfa591db35ab7284f09d0b826c408d3c
CECurtiss <curtissclinton@gmail.com>
2022-12-28 21:30:47 -0600

    added file structure. wrote connection.js, server.js, schema

---
+++ server.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const express = require('express');
+const session = require('session');
+
+const routes = require('./controllers');
+
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Set up Handlebars.js engine with custom helpers
+
+
+const sessionConfig = {
+  secret: '',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sessionConfig));
+
+// Inform Express.js on which template engine to use
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening on http://localhost:3001'));
+});
