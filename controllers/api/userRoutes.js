4a2afd51b03da8426eb99a1d8f2dd519d00cadc5
CECurtiss <curtissclinton@gmail.com>
2022-12-29 21:33:46 -0600

    completed route for creating new user

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,10 +1,39 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
+// route to add user
 router.post('/', async (req, res) => {
     try {
 
     } catch (err) {
+        res.status(400).json(err)
+    }
+})
+
+// route to login existing user
+router.post('/login', async (req, res) => {
+    try {
+        const userData = await User.findOne({ where: {userName: req.body.userName} });
+
+        if(!userData) {
+            res.status(400).json({ message: 'Incorrect Username or Password. Please try again.' });
+            return;
+        }
+
+        const validatePassword = await userData.checkPassword(req.body.password);
+
+        if (!validatePassword) {
+            res.status(400).json({ message: 'Incorrect Username or Password. Please try again'});
+            return;
+        }
+
+        req.session.save(() => {
+            req.session.user_id = userData.id;
+            req.session.logged_in = true;
+
+            res.json({ user: userData, message: 'Your are logged in'})
+        })
+    } catch(err) {
         res.status(400).json(err)
     }
 })
