d2719acaa334c46da20b1d8b6420f39d91a591f6
CECurtiss <curtissclinton@gmail.com>
2022-12-29 22:19:32 -0600

    added route info for index.js in controllers and controllers/api

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const { BlogPost, User } = require('../models');
 
+// get all blog posts for homepage
 router.get('/', async (req, res) => {
     try {
         const dbBlogPostData = await BlogPost.findAll({
@@ -15,9 +16,10 @@ router.get('/', async (req, res) => {
         blogs.get({ plain: true })
         );
 
-        res.render('homepage', allBlogPosts)
-    
-    
+        res.render('homepage', {
+            allBlogPosts,
+            loggedIn: req.session.loggedIn,
+        })  
 } catch (err) {
     console.log(err);
     res.status(500).json(err);
