203faa4a356532a638d880a8beda9ef7474c3f81
CECurtiss <curtissclinton@gmail.com>
2023-01-23 18:05:47 -0600

    fixed logout problem

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -58,7 +58,7 @@ router.post("/login", async (req, res) => {
 // route to logout
 router.post("/logout", (req, res) => {
   if (req.session.logged_in) {
-    res.session.destroy(() => {
+    req.session.destroy(() => {
       res.status(204).end();
     });
   } else {
