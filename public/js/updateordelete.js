ae8fe94dc2599b63cc55922b24f7c540ad254db9
CECurtiss <curtissclinton@gmail.com>
2023-01-25 20:50:10 -0600

    push for BCBS

--- public/js/updateordelete.js
+++ public/js/updateordelete.js
@@ -3,7 +3,8 @@ const updatePost = async (event) => {
   
     const title = document.querySelector("#editposttitleform").value.trim();
     const content = document.querySelector("#editpostcontentform").value.trim();
-  
+    const postID =
+
     if (title && content) {
       const response = await fetch("/api/blogs/", {
         method: "PUT",
