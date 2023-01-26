87cc1780e30aa208d9f020463a0580da23ed0ecb
CECurtiss <curtissclinton@gmail.com>
2023-01-25 20:49:31 -0600

    pushing before Ask BCBS

---
+++ public/js/updateordelete.js
@@ -0,0 +1,22 @@
+const updatePost = async (event) => {
+    event.preventDefault();
+  
+    const title = document.querySelector("#editposttitleform").value.trim();
+    const content = document.querySelector("#editpostcontentform").value.trim();
+  
+    if (title && content) {
+      const response = await fetch("/api/blogs/", {
+        method: "PUT",
+        body: JSON.stringify({ title, content }),
+        headers: { "Content-Type": "application/json" },
+      });
+  
+      if (response.ok) {
+        document.location.replace("/dashboard");
+      } else {
+        alert("Failed to update post");
+      }
+    }
+  };
+  
+  document.querySelector("#updatepostbtn").addEventListener("click", updatePost);
